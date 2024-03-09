857d70bb5abdeab52a52a6832d7470526de80ff3
frenki4141 <frankbaraka589@gmail.com>
2024-03-09 03:15:52 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,25 @@
+void main() {
+  // Demonstrating int data type
+  int age = 25;
+  print('Age: $age');
+
+  // Demonstrating double data type
+  double weight = 65.5;
+  print('Weight: $weight');
+
+  // Demonstrating String data type
+  String name = 'frank baraka';
+  print('Name: $name');
+
+  // Demonstrating List data type
+  List<int> numbers = [1, 2, 3, 4, 5];
+  print('Numbers: $numbers');
+
+  // Demonstrating Map data type
+  Map<String, dynamic> person = {
+    'name': 'frank',
+    'age': 30,
+    'isMarried': true,
+  };
+  print('Person: $person');
+}
